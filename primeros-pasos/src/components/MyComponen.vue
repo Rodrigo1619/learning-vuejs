f3570bcdfd6b64e5bde01dca32a671efa8b5a9c4
Rodrigo1619 <00067920@uca.edu.sv>
2025-12-30 17:41:21 -0600

    primeros pasos vue

---
+++ primeros-pasos/src/components/MyComponen.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="my-componen">
+        <!-- <h1 v-if="isVisible">{{ message }}</h1> -->
+        <!-- <button v-on:click="sayHello">Toggle visibility</button> -->
+        <!-- <button @click="sayHello">Toggle visibility</button> --> <!-- @click es lo mismo que v-on:click -->
+
+
+        <!-- bind sirve para determinar si la clase highlighted se aplica o no -->
+        <h1 v-bind:class="{ highlighted: isHighlighted }">{{ tittle }}</h1>
+        <button @click="toggleHighlight">Toggle Highlight</button>
+        <input v-model="message">
+        <!--enlace bidireccional (actualiza message) actualiza lo del input y el dom al misma tiempo  -->
+        <ul>
+            <li v-for="value in list" :key="value.id">{{ value.name }}</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponen',
+    data() {
+        return {
+            message: 'Hola desde MyComponen.vue',
+            isVisible: true,
+            tittle: 'Listado de elementos',
+            isHighlighted: false,
+            list: [
+                { id: 1, name: 'Elemento 1' },
+                { id: 2, name: 'Elemento 2' },
+                { id: 3, name: 'Elemento 3' }
+            ]
+        }
+    },
+    methods: {
+        toggleVisibility() {
+            this.isVisible = !this.isVisible;
+        },
+        sayHello() {
+            alert("Hello!");
+            //console.log("message", this.message);
+        },
+        toggleHighlight() {
+            this.isHighlighted = !this.isHighlighted;
+        }
+    }
+}
+</script>
+
+<style>
+    .highlighted {
+        color: yellow;
+    }
+</style>
---
+++ primeros-pasos/src/components/newComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <h1>
+            {{ message }}
+            <button @click="changeMessage">Change message</button>
+        </h1>
+    </div>
+</template>
+
+<script>
+    import {mapState, mapMutations} from 'vuex';
+    export default {
+        computed:{
+            ...mapState(['message'])
+        },
+        methods:{
+            ...mapMutations(['setMessage']),
+            changeMessage(){
+                this.setMessage('New message from newComponent.vue');
+            }
+        }
+    }
+</script>
